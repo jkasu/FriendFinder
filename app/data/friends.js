64f9f9bc23c5e524071aa0b66f8ec52a2f467e88
jkasu <jyothsna_in@yahoo.com>
2018-05-22 13:28:02 -0400

    Friend Finder

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Dino",
+		photo: "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/13902821_10205112947503190_1593172292100109413_n.jpg?_nc_cat=0&oh=43256e07ef1525210fe7897ed97d10d4&oe=5B925B15",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Pakku",
+		photo: "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/31351426_10155754798041387_7099952350663740310_n.jpg?_nc_cat=0&oh=14b41a60541da45811c31ecf44894976&oe=5B884014",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Sonnu",
+		photo: "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/167509_1685543670259_7851255_n.jpg?_nc_cat=0&oh=1dd7527b9deab93ea331b988b87fd95a&oe=5BC5256F",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Paddi",
+		photo: "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/10407940_10205500367007136_1505296563806572305_n.jpg?_nc_cat=0&oh=fb7be1c23930584b53dcc60381cabbcc&oe=5B93D265",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Chetu",
+		photo: "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/988458_10152023828760723_233865115801390095_n.jpg?_nc_cat=0&oh=c4f28f371978cd589fc375ae4ebdd423&oe=5B7890A1",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
